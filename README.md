87ce9e34c835d972f7b927f006b0186830a4262b
Dave Brown <key88sf@gmail.com>
2020-01-13 14:29:26 -0500

    Update based on tap-pagerduty code.

--- README.md
+++ README.md
@@ -1,59 +1,114 @@
 # tap-swellrewards
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Python Versions](https://img.shields.io/badge/python-3.6%20%7C%203.7-blue.svg)](https://pypi.python.org/pypi/ansicolortags/)
 
-This is a [Singer](https://singer.io) tap that produces JSON-formatted data
-following the [Singer spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md).
+A [Singer](https://www.singer.io/) tap for extracting data from the [Swell Rewards API](https://loyaltyapi.yotpo.com/reference).
 
-This tap:
+## Installation
 
-- Pulls raw data from [Swell Rewards](https://loyaltyapi.yotpo.com/reference)
-- Extracts the following resources:
-  - [customers](https://loyaltyapi.yotpo.com/reference#customers)
-- Outputs the schema for each resource
+Since package dependencies tend to conflict between various taps and targets, Singer [recommends](https://github.com/singer-io/getting-started/blob/master/docs/RUNNING_AND_DEVELOPING.md#running-singer-with-python) installing taps and targets into their own isolated virtual environments:
 
-## Quick Start
+### Install SwellRewards Tap
 
-1. Install
+```bash
+$ cd tap-swellrewards
+$ python3 -m venv ~/.venvs/tap-swellrewards
+$ source ~/.venvs/tap-swellrewards/bin/activate
+$ pip3 install tap-swellrewards
+$ deactivate
+```
 
-    ```bash
-    $ pip install tap-swellrewards
-    ```
+### Install Singer Target
 
-2. Get API keys
+```bash
+$ python3 -m venv ~/.venvs/target-stitch
+$ source ~/.venvs/target-stitch/bin/activate
+$ pip3 install target-stitch
+$ deactivate
+```
 
-    You can find your `api_key` and `GUID` in your Swell Rewards settings.
+## Configuration
 
+The tap accepts a JSON-formatted configuration file as arguments. This configuration file has three required fields:
 
-3. Create the config file
+1. `api_key`: A valid Swell Rewards API Key (found in the Settings of your Swell account)
+2. `api_guid`: A valid Swell Rewards API GUID (found in the Settings of your Swell account)
+3. `last_seen_at` The `last_seen_at` parameter determines the starting date for the last registered customer activity within the Yotpo loyalty and rewards system e.g. purchase, redemption, etc. For example, pass a value of `2019-01-01` to fetch active customers since January 1st, 2019. 
 
-   You must create a JSON configuration file that looks like this:
+An bare-bones SwellRewards confirguration may file may look like the following:
 
-   ```json
-   {
-       "last_seen_at": "2015-01-01",
-       "api_key": "...",
-       "api_guid": "..."
-   }
-   ```
+```json
+{
+  "api_key": "abaoEFoiahwef12425",
+  "api_guid": "YAYAHbafoiafoH235",
+  "last_seen_at": "2019-01-21"
+}
+```
 
-   The `last_seen_at` parameter determines the starting date for the last registered
-   customer activity within the Yotpo loyalty and rewards system e.g. purchase, redemption, etc. 
-   For example, pass a value of `2019-01-01` to fetch active customers since January 1st, 2019.
+## Streams
 
-4. Run the Tap in Discovery Mode
+The current version of the tap syncs one distinct [Stream](https://github.com/singer-io/getting-started/blob/master/docs/SYNC_MODE.md#streams):
+1. `Customers`: ([Endpoint](https://loyaltyapi.yotpo.com/reference#customers))
 
-    ```bash
-    $ tap-swellrewards -c config.json -d
-    ```
+## Discovery
 
-   See the Singer docs on discovery mode
-   [here](https://github.com/singer-io/getting-started/blob/master/BEST_PRACTICES.md#discover-mode-and-connection-checks).
+Singer taps describe the data that a stream supports via a [Discovery](https://github.com/singer-io/getting-started/blob/master/docs/DISCOVERY_MODE.md#discovery-mode) process. You can run the SwellRewards tap in Discovery mode by passing the `--discover` flag at runtime:
 
-5. Run the Tap in Sync Mode
+```bash
+$ ~/.venvs/tap-swellrewards/bin/tap-swellrewards --config=config/swellrewards.config.json --discover
+```
 
-    ```bash
-    $ tap-swellrewards -c config.json -p catalog-file.json
-    ```
+The tap will generate a [Catalog](https://github.com/singer-io/getting-started/blob/master/docs/DISCOVERY_MODE.md#the-catalog) to stdout. To pass the Catalog to a file instead, simply redirect it to a file:
 
----
+```bash
+$ ~/.venvs/tap-swellrewards/bin/tap-swellrewards --config=config/swellrewards.config.json --discover > catalog.json
+```
+
+## Sync Locally
+
+Running a tap in [Sync mode](https://github.com/singer-io/getting-started/blob/master/docs/SYNC_MODE.md#sync-mode) will extract data from the various Streams. In order to run a tap in Sync mode, pass a configuration file and catalog file:
+
+```bash
+$ ~/.venvs/tap-swellrewards/bin/tap-swellrewards --config=config/swellrewards.config.json --catalog=catalog.json
+```
+
+The tap will emit occasional [State messages](https://github.com/singer-io/getting-started/blob/master/docs/SPEC.md#state-message). You can persist State between runs by redirecting State to a file:
+
+```bash
+$ ~/.venvs/tap-swellrewards/bin/tap-swellrewards --config=config/swellrewards.config.json --catalog=catalog.json >> state.json
+$ tail -1 state.json > state.json.tmp
+$ mv state.json.tmp state.json
+```
+
+To pick up from where the tap left off on subsequent runs, simply supply the [State file](https://github.com/singer-io/getting-started/blob/master/docs/CONFIG_AND_STATE.md#state-file) at runtime:
+
+```bash
+$ ~/.venvs/tap-swellrewards/bin/tap-swellrewards --config=config/swellrewards.config.json --catalog=catalog.json --state=state.json >> state.json
+$ tail -1 state.json > state.json.tmp
+$ mv state.json.tmp state.json
+```
+
+## Sync to Stitch
+
+You can also send the output of the tap to [Stitch Data](https://www.stitchdata.com/) for loading into the data warehouse. To do this, first create a JSON-formatted configuration for Stitch. This configuration file has two required fields:
+1. `client_id`: The ID associated with the Stitch Data account you'll be sending data to.
+2. `token` The token associated with the specific [Import API integration](https://www.stitchdata.com/docs/integrations/import-api/) within the Stitch Data account.
+
+An example configuration file will look as follows:
+
+```json
+{
+  "client_id": 1234,
+  "token": "foobarfoobar"
+}
+```
+
+Once the configuration file is created, simply pipe the output of the tap to the Stitch Data target and supply the target with the newly created configuration file:
+
+```bash
+$ ~/.venvs/tap-swellrewards/bin/tap-swellrewards --config=config/swellrewards.config.json --catalog=catalog.json --state=state.json | ~/.venvs/target-stitch/bin/target-stitch --config=config/stitch.config.json >> state.json
+$ tail -1 state.json > state.json.tmp
+$ mv state.json.tmp state.json
+```
 
 Copyright &copy; 2020 Stitch
--- tap_swellrewards/streams.py
+++ tap_swellrewards/streams.py
@@ -1,140 +1,312 @@
+import inspect
+import os
+import time
+from datetime import datetime, timedelta
+from typing import Dict
+
+import requests
 import singer
-from singer import metrics, transform
-import pendulum
 
 LOGGER = singer.get_logger()
 
-PAGE_SIZE = 100
 
-class Stream(object):
-    def __init__(self, tap_stream_id, pk_fields, path,
-                 returns_collection=True,
-                 collection_key=None,
-                 pluck_results=False,
-                 custom_formatter=None,
-                 version=None):
-        self.tap_stream_id = tap_stream_id
-        self.pk_fields = pk_fields
-        self.path = path
-        self.returns_collection = returns_collection
-        self.collection_key = collection_key
-        self.pluck_results = pluck_results
-        self.custom_formatter = custom_formatter or (lambda x: x)
-        self.version = version
+class SwellRewardsStream:
+    BASE_URL = "https://app.swellrewards.com/api/v2"
 
-        self.start_date = None
-
-    def get_start_date(self, ctx, key):
-        if not self.start_date:
-            self.start_date = ctx.get_bookmark([self.tap_stream_id, key])
-        return self.start_date
-
-    def metrics(self, records):
-        with metrics.record_counter(self.tap_stream_id) as counter:
-            counter.increment(len(records))
-
-    def write_records(self, records):
-        singer.write_records(self.tap_stream_id, records)
-        self.metrics(records)
-
-    def format_response(self, response):
-        if self.pluck_results:
-            response = response['response']
-
-        if self.returns_collection:
-            if self.collection_key:
-                records = (response or {}).get(self.collection_key, [])
-            else:
-                records = response or []
-        else:
-            records = [] if not response else [response]
-        return self.custom_formatter(records)
-
-
-class Paginated(Stream):
-    def get_params(self, ctx, page):
-        return {
-            "per_page": PAGE_SIZE,
-            "page": page
+    def __init__(self, config, state):
+        self.config = config
+        self.api_key = config.get('api_key')
+        self.api_guid = config.get('api_guid')
+        self.state = state
+        self.params = {
+            "per_page": config.get('per_page', 100),
+            "page": 1,
+            "last_seen_at": config.get('last_seen_at')
         }
+        self.schema = self.load_schema()
+        self.metadata = singer.metadata.get_standard_metadata(schema=self.load_schema(),
+                                                              key_properties=self.key_properties,
+                                                              valid_replication_keys=self.valid_replication_keys,
+                                                              replication_method=self.replication_method)
 
-    def on_batch_complete(self, ctx, records, product_id=None):
-        self.write_records(records)
-        return True
+        config_stream_params = config.get('streams', {}).get(self.tap_stream_id)
 
-    def _sync(self, ctx, path=None, product_id=None):
-        if path is None:
-            path = self.path
+        if config_stream_params is not None:
+            for key in config_stream_params.keys():
+                if key not in self.valid_params:
+                    raise RuntimeError(f"/{self.tap_stream_id} endpoint does not support '{key}' parameter.")
 
-        if product_id:
-            bookmark_name = 'product_{}.since_date'.format(product_id)
+            self.params.update(config_stream_params)
+
+        for param in self.required_params:
+            if param not in self.params.keys():
+                if param == 'until':
+                    self.params.update({"until": datetime.strftime(datetime.utcnow(), '%Y-%m-%dT%H:%M:%SZ')})
+                else:
+                    raise RuntimeError(f"Parameter '{param}' required but not supplied for /{self.tap_stream_id} endpoint.")
+
+    def get(self, key: str):
+        '''Custom get method so that Singer can
+        access Class attributes using dict syntax.
+        '''
+        return inspect.getattr_static(self, key, default=None)
+
+    def _get_abs_path(self, path: str) -> str:
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
+    def load_schema(self) -> Dict:
+        '''Loads a JSON schema file for a given
+        SwellRewards resource into a dict representation.
+        '''
+        schema_path = self._get_abs_path("schemas")
+        return singer.utils.load_json(f"{schema_path}/{self.tap_stream_id}.json")
+
+    def write_schema(self):
+        '''Writes a Singer schema message.'''
+        return singer.write_schema(stream_name=self.stream, schema=self.schema, key_properties=self.key_properties)
+
+    def write_state(self):
+        return singer.write_state(self.state)
+
+    def _construct_headers(self) -> Dict:
+        headers = requests.utils.default_headers()
+        headers["User-Agent"] = "python-swellrewards-tap"
+        headers["x-guid"] = self.api_guid
+        headers["x-api-key"] = self.api_key
+        headers["Content-Type"] = "application/json"
+        return headers
+
+    def _get(self, url_suffix: str, params: Dict = None) -> Dict:
+        url = self.BASE_URL + url_suffix
+        headers = self._construct_headers()
+        response = requests.get(url, headers=headers, params=params)
+        if response.status_code == 429:
+            LOGGER.warn("Rate limit reached. Trying again in 60 seconds.")
+            time.sleep(60)
+            response = requests.get(url, headers=headers, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def update_bookmark(self, bookmark, value):
+        if bookmark is None:
+            new_bookmark = value
         else:
-            bookmark_name = 'since_date'
-        ctx.update_start_date_bookmark([self.tap_stream_id, bookmark_name])
+            new_bookmark = max(bookmark, value)
+        return new_bookmark
 
-        schema = ctx.catalog.get_stream(self.tap_stream_id).schema.to_dict()
-
-        page = 1
-        while True:
-            params = self.get_params(ctx, page)
-            opts = {"path": path, "params": params}
-            resp = ctx.client.GET(self.version, opts, self.tap_stream_id)
-            raw_records = self.format_response(resp)
-            records = [transform(record, schema) for record in raw_records]
-
-            if not self.on_batch_complete(ctx, records, product_id):
-                break
-
-            if len(records) == 0:
-                break
-            page += 1
-
-    def sync(self, ctx):
-        self._sync(ctx)
-
-    def _transform_dt(self, time_str):
-        return pendulum.parse(time_str).in_timezone("UTC")
-
-    def update_bookmark(self, ctx, max_record_ts, path_key):
-        path = [self.tap_stream_id, path_key]
-        bookmark_ts = self._transform_dt(ctx.get_bookmark(path))
-
-        last_record_ts = self._transform_dt(max_record_ts)
-
-        if last_record_ts > bookmark_ts:
-            ctx.set_bookmark(path, last_record_ts.to_date_string())
+    def _list_resource(self, url_suffix: str, params: Dict = None):
+        response = self._get(url_suffix=url_suffix, params=params)
+        return SwellRewardsResponse(self, url_suffix, params, response)
 
 
-class Customers(Paginated):
-    def get_params(self, ctx, page):
-        last_seen_at_raw = self.get_start_date(ctx, 'last_seen_at')
-        last_seen_at = pendulum.parse(last_seen_at_raw).in_timezone("UTC")
+class SwellRewardsResponse:
+    def __init__(self, client, url_suffix, params, response):
+        self.client = client
+        self.url_suffix = url_suffix
+        self.params = params
+        self.response = response
 
-        return {
-            "per_page": PAGE_SIZE,
-            "page": page,
-            "last_seen_at": last_seen_at.to_iso8601_string(),
-        }
+    def __iter__(self):
+        self._iteration = 0
+        return self
 
-    def on_batch_complete(self, ctx, records, product_id=None):
-        self.write_records(records)
+    def __next__(self):
+        self._iteration += 1
+        if self._iteration == 1:
+            return self
 
-        if len(records) == 0:
-            return False
+        if self.response.get("links") is None:
+            raise StopIteration
 
-        last_record = records[-1]
-        max_record_ts = last_record['last_seen_at']
-        self.update_bookmark(ctx, max_record_ts, 'since_date')
+        if self.response.get("links")["total_pages"] > self.params["page"]:
+            self.params["page"] += 1
+            self.response = self.client._get(
+                url_suffix=self.url_suffix, params=self.params
+            )
+            return self
+        else:
+            raise StopIteration
 
-        return True
+    def get(self, key, default=None):
+        return self.response.get(key, default)
 
-all_streams = [
 
-    Customers(
-        "customers",
-        ["email"],
-        "customers/all",
-        collection_key="customers"
-    )
-]
-all_stream_ids = [s.tap_stream_id for s in all_streams]
+class IncidentsStream(SwellRewardsStream):
+    tap_stream_id = 'incidents'
+    stream = 'incidents'
+    key_properties = 'id'
+    replication_key = 'last_status_change_at'
+    valid_replication_keys = ['last_status_change_at']
+    replication_method = 'FULL_TABLE'
+    valid_params = [
+        'start_date',
+        'until',
+        'date_range',
+        'statuses[]',
+        'incident_key',
+        'service_ids[]',
+        'team_ids[]',
+        'user_ids[]',
+        'urgencies[]',
+        'time_zone',
+        'sort_by',
+        'include[]'
+    ]
+    required_params = ['until']
+
+    def __init__(self, config, state, **kwargs):
+        super().__init__(config, state)
+
+    def sync(self):
+        current_bookmark = singer.bookmarks.get_bookmark(state=self.state,
+                                                         tap_stream_id=self.tap_stream_id,
+                                                         key=self.replication_key,
+                                                         default=None)
+
+        if current_bookmark is not None:
+            current_bookmark_dtime = datetime.strptime(current_bookmark, '%Y-%m-%dT%H:%M:%SZ')
+        else:
+            current_bookmark_dtime = None
+
+        since_dtime = datetime.strptime(self.params.get("start_date"), '%Y-%m-%dT%H:%M:%SZ')
+        until_dtime = datetime.strptime(self.params.get("until"), '%Y-%m-%dT%H:%M:%SZ')
+        request_range_limit = timedelta(days=179)
+
+        running_bookmark_dtime = None
+        with singer.metrics.job_timer(job_type=f"list_{self.tap_stream_id}"):
+            with singer.metrics.record_counter(endpoint=self.tap_stream_id) as counter:
+                while since_dtime < until_dtime:
+                    range = {
+                        "offset": 0,  # Reset the offset each time.
+                        "start_date": datetime.strftime(since_dtime, '%Y-%m-%dT%H:%M:%SZ'),
+                        "until": datetime.strftime(min(since_dtime + request_range_limit, until_dtime), '%Y-%m-%dT%H:%M:%SZ')
+                    }
+                    self.params.update(range)
+                    for page in self._list_resource(url_suffix=f"/{self.tap_stream_id}", params=self.params):
+                        for record in page.get(self.tap_stream_id):
+                            record_replication_key_dtime = datetime.strptime(record.get(self.replication_key), '%Y-%m-%dT%H:%M:%SZ')
+
+                            substream_params = {
+                                "limit": 100,
+                                "offset": 0,
+                                "time_zone": "UTC"
+                            }
+                            record['log_entries'] = []
+                            for page in self._list_resource(url_suffix=f"/{self.tap_stream_id}/{record.get('id')}/log_entries", params=substream_params):
+                                record['log_entries'].extend(page.get('log_entries'))
+
+                            record['alerts'] = []
+                            for page in self._list_resource(url_suffix=f"/{self.tap_stream_id}/{record.get('id')}/alerts", params=substream_params):
+                                record['alerts'].extend(page.get('alerts'))
+
+                            if self.replication_method == 'INCREMENTAL':
+                                if (current_bookmark_dtime is None) or (record_replication_key_dtime >= current_bookmark_dtime):
+                                    with singer.Transformer() as transformer:
+                                        transformed_record = transformer.transform(data=record, schema=self.schema)
+                                        singer.write_record(stream_name=self.stream, time_extracted=singer.utils.now(), record=transformed_record)
+                                        counter.increment()
+                                        running_bookmark_dtime = self.update_bookmark(running_bookmark_dtime, record_replication_key_dtime)
+                            else:
+                                with singer.Transformer() as transformer:
+                                    transformed_record = transformer.transform(data=record, schema=self.schema)
+                                    singer.write_record(stream_name=self.stream, time_extracted=singer.utils.now(), record=transformed_record)
+                                    counter.increment()
+
+                    since_dtime += request_range_limit
+
+        if self.replication_method == 'INCREMENTAL':
+            running_bookmark_str = datetime.strftime(running_bookmark_dtime, '%Y-%m-%dT%H:%M:%SZ')
+            singer.bookmarks.write_bookmark(state=self.state,
+                                            tap_stream_id=self.tap_stream_id,
+                                            key=self.replication_key,
+                                            val=running_bookmark_str)
+
+
+class CustomersStream(SwellRewardsStream):
+    tap_stream_id = 'customers'
+    stream = 'customers'
+    key_properties = 'email'
+    valid_replication_keys = []
+    replication_method = 'FULL_TABLE'
+    valid_params = [
+        'page',
+        'per_page',
+        'last_seen_at',
+    ]
+    required_params = ['last_seen_at']
+
+    def __init__(self, config, state, **kwargs):
+        super().__init__(config, state)
+
+    def sync(self):
+
+        with singer.metrics.job_timer(job_type=f"list_{self.tap_stream_id}"):
+            with singer.metrics.record_counter(endpoint=self.tap_stream_id) as counter:
+                for page in self._list_resource(url_suffix="/customers/all", params=self.params):
+                    for record in page.get(self.tap_stream_id):
+                        with singer.Transformer() as transformer:
+                            transformed_record = transformer.transform(data=record, schema=self.schema)
+                            singer.write_record(stream_name=self.stream, time_extracted=singer.utils.now(), record=transformed_record)
+                            counter.increment()
+
+
+class NotificationsStream(SwellRewardsStream):
+    tap_stream_id = 'notifications'
+    stream = 'notifications'
+    key_properties = 'id'
+    replication_key = 'started_at'
+    valid_replication_keys = ['started_at']
+    replication_method = 'INCREMENTAL'
+    valid_params = ['time_zone', 'start_date', 'until', 'filter', 'include']
+    required_params = ['start_date', 'until']
+
+    def __init__(self, config, state):
+        super().__init__(config, state)
+
+    def sync(self):
+        current_bookmark = singer.bookmarks.get_bookmark(state=self.state,
+                                                         tap_stream_id=self.tap_stream_id,
+                                                         key=self.replication_key,
+                                                         default=None)
+
+        if current_bookmark is not None:
+            current_bookmark_dtime = datetime.strptime(current_bookmark, '%Y-%m-%dT%H:%M:%SZ')
+        else:
+            current_bookmark_dtime = None
+
+        since_dtime = datetime.strptime(self.params.get("start_date"), '%Y-%m-%dT%H:%M:%SZ')
+        until_dtime = datetime.strptime(self.params.get("until"), '%Y-%m-%dT%H:%M:%SZ')
+        request_range_limit = timedelta(days=89)
+
+        running_bookmark_dtime = None
+        with singer.metrics.job_timer(job_type=f"list_{self.tap_stream_id}"):
+            with singer.metrics.record_counter(endpoint=self.tap_stream_id) as counter:
+                while since_dtime < until_dtime:
+                    range = {
+                        "offset": 0,  # Reset the offset each time.
+                        "start_date": datetime.strftime(since_dtime, '%Y-%m-%dT%H:%M:%SZ'),
+                        "until": datetime.strftime(min(since_dtime + request_range_limit, until_dtime), '%Y-%m-%dT%H:%M:%SZ')
+                    }
+                    self.params.update(range)
+                    for page in self._list_resource(url_suffix=f"/{self.tap_stream_id}", params=self.params):
+                        for record in page.get(self.tap_stream_id):
+                            record_replication_key_dtime = datetime.strptime(record.get(self.replication_key), '%Y-%m-%dT%H:%M:%SZ')
+                            if (current_bookmark_dtime is None) or (record_replication_key_dtime >= current_bookmark_dtime):
+                                with singer.Transformer() as transformer:
+                                    transformed_record = transformer.transform(data=record, schema=self.schema)
+                                    singer.write_record(stream_name=self.stream, time_extracted=singer.utils.now(), record=transformed_record)
+                                    counter.increment()
+                                    running_bookmark_dtime = self.update_bookmark(running_bookmark_dtime, record_replication_key_dtime)
+
+                    since_dtime += request_range_limit
+
+        running_bookmark_str = datetime.strftime(running_bookmark_dtime, '%Y-%m-%dT%H:%M:%SZ')
+        singer.bookmarks.write_bookmark(state=self.state,
+                                        tap_stream_id=self.tap_stream_id,
+                                        key=self.replication_key,
+                                        val=running_bookmark_str)
+
+
+AVAILABLE_STREAMS = {
+    CustomersStream
+}
