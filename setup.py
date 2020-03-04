d622aac0b73f84174d769c758705e06efcf081a5
Andy Lu <andy@stitchdata.com>
2020-03-04 15:35:05 -0500

    Bump to v0.1.3 (#5)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-swellrewards",
-    version="0.1.2",
+    version="0.1.3",
     description="Singer.io tap for extracting data from Swell Rewards API",
     author="Stitch",
     url="http://singer.io",
